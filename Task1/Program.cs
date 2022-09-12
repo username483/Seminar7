d7a44918c8cc2f2d61abccd3ca4fc2c0ce3441c4
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-12 18:45:22 +0300

    решена первая задачана создание и вывод массива

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив размером mxn
+// заполненный случайными целыми числами
+
+Console.Clear();
+
+Console.Write("количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, 1, 10);
+PrintArray(array);
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return array;
+}
+
+void PrintArray(int[,] filledArray)
+{
+    for (int i = 0; i < filledArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < filledArray.GetLength(1); j++)
+        {
+            Console.Write(filledArray[i, j]+ " ");
+        }
+          Console.WriteLine();
+    }
+
+}
+
