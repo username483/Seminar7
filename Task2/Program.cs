584fe8c5da0b715f8dce62da910f1d9eda14f556
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-12 20:25:59 +0300

    вывести двумерный массив из сумм индексов столбца и строки

---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+Console.Clear();
+
+Console.Write("количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = FillArray(rows, columns);
+PrintArray(array);
+
+int[,] FillArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i+j;
+        }
+
+    }
+    return array;
+}
+
+void PrintArray(int[,] filledArray)
+{
+    for (int i = 0; i < filledArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < filledArray.GetLength(1); j++)
+        {
+            Console.Write(filledArray[i, j]+ " ");
+        }
+          Console.WriteLine();
+    }
+
+}
+
